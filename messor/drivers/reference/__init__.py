9201f5071f39bb0a1b8a2164a31400c08ad3c4dd
Rick van de Loo <rickvandeloo@gmail.com>
2015-09-20 15:47:34 +0200

    Implement drop_off logic

--- messor/drivers/reference/__init__.py
+++ messor/drivers/reference/__init__.py
@@ -1,5 +1,5 @@
 import os
-from messor.utils import ensure_directory
+from messor.utils import ensure_directory, list_all_files
 from messor.settings import FORAGER_BUFFER, FORMICARY_PATH
 
 # This driver uses individual files written to a directory structure that is
@@ -16,3 +16,23 @@ class ChecksumFilesDriver(object):
         ensure_directory(os.path.dirname(reference_path))
         with open(reference_path, 'w') as f:
             f.write(checksum)
+
+    def _read_checksum_from_file(self, filename):
+        with open(filename, 'rb') as f:
+            return f.read()
+
+    def _references_path_from_host(self, host):
+        return os.path.join(FORAGER_BUFFER, 'hosts', host)
+
+    def _list_all_references_for_host(self, host):
+        references_path = self._references_path_from_host(host)
+        return list_all_files(references_path)
+
+    def _remove_buffer_path_from_filename(self, filename):
+        return filename.replace(os.path.join(FORAGER_BUFFER, 'hosts'), '')
+
+    def file_index_for_host(self, host):
+        references = self._list_all_references_for_host(host)
+        filenames = map(self._remove_buffer_path_from_filename, references)
+        checksums = map(self._read_checksum_from_file, references)
+        return zip(filenames, checksums)
--- messor/forager/drop_off.py
+++ messor/forager/drop_off.py
@@ -1,2 +1,27 @@
+import os
+
+from messor.utils import list_directories
+from messor.settings import FORAGER_BUFFER
+from messor.drivers.transfer import FlatBufferDriver
+from messor.drivers.reference import ChecksumFilesDriver
+
+reference_driver = ChecksumFilesDriver()
+transfer_driver = FlatBufferDriver()
+
+def process_file(file_entry):
+    filename, checksum = file_entry
+    transfer_driver.ensure_file_in_inbox(filename, checksum)
+    reference_file = os.path.join(FORAGER_BUFFER, 'hosts') + filename
+    os.remove(reference_file)
+
+def sync_to_inbox(host):
+    file_entries = reference_driver.file_index_for_host(host)
+    map(process_file, file_entries)
+
+def list_buffer_hosts():
+    buffer_path = os.path.join(FORAGER_BUFFER, 'hosts')
+    return list_directories(buffer_path)
+
 def drop_off():
-    pass
+    hosts = list_buffer_hosts()
+    map(sync_to_inbox, hosts)
