b9ace2285852ea8eaaabb50e5c2cd1051d124731
Rick van de Loo <rickvandeloo@gmail.com>
2015-10-28 18:09:00 +0100

    move flush_buffer to buffer.py

--- messor/drop_off.py
+++ messor/drop_off.py
@@ -1,22 +1,15 @@
 import os
 import logging
-from itertools import chain
 from concurrent.futures import ThreadPoolExecutor
 
-from messor.utils import list_directories
-from messor.settings import MESSOR_BUFFER
 from messor.drivers.reference import ChecksumFilesDriver
 from messor.drivers.remote import SshDriver
+from messor.buffer import flush_buffer, list_buffer_hosts
+from messor.settings import MESSOR_BUFFER
 
 logger = logging.getLogger(__name__)
 
 reference_driver = ChecksumFilesDriver()
-
-def flush_buffer():
-    hosts = list_buffer_hosts()
-    zipped_lists = zip(*chain(*map(reference_driver.file_index_for_host, hosts)))
-    unresolved_checksums = zipped_lists[1] if zipped_lists else []
-    reference_driver.purge_buffer(unresolved_checksums)
 
 def process_file(file_entry, remote_driver):
     filename, checksum = file_entry
@@ -38,11 +31,6 @@ def process_host(host):
         sync_to_inbox(host, remote_driver)
     except EOFError:
 	logger.debug("Host %s not in range, skipping!" % host)
-
-def list_buffer_hosts():
-    logger.debug("Generating a list of buffer host directories")
-    buffer_path = os.path.join(MESSOR_BUFFER, 'hosts')
-    return list_directories(buffer_path)
 
 def drop_off():
     flush_buffer()
--- tests/unit/messor/test_drop_off.py
+++ tests/unit/messor/test_drop_off.py
@@ -2,9 +2,9 @@ from unittest import TestCase
 from mock import patch, call, Mock
 from types import GeneratorType
 
-from messor.settings import MESSOR_BUFFER
 from messor.drop_off import drop_off, list_buffer_hosts, sync_to_inbox, \
-    flush_buffer, process_file, process_host
+    process_file, process_host
+from messor.settings import MESSOR_BUFFER
 
 class TestProcessFile(TestCase):
     def setUp(self):
@@ -43,45 +43,6 @@ class TestProcessFile(TestCase):
 
         self.flush_buffer.assert_called_once_with()
 
-
-class TestFlushBuffer(TestCase):
-    def setUp(self):
-        patcher = patch('messor.drop_off.list_buffer_hosts')
-        self.addCleanup(patcher.stop)
-        self.list_buffer_hosts = patcher.start()
-        self.fake_hosts = ['host1', 'host2', 'host3']
-        self.list_buffer_hosts.return_value = self.fake_hosts
-
-        patcher = patch('messor.drivers.reference.ChecksumFilesDriver.file_index_for_host')
-        self.addCleanup(patcher.stop)
-        self.file_index_for_host = patcher.start()
-        self.file_index_for_host.side_effect = [
-            [('filename1', 'checksum1')],
-            [('filename2', 'checksum2')],
-            [('filename3', 'checksum3')]
-        ]
-
-        patcher = patch('messor.drivers.reference.ChecksumFilesDriver.purge_buffer')
-        self.addCleanup(patcher.stop)
-        self.purge_buffer = patcher.start()
-
-    def test_flush_buffer_lists_buffer_hosts(self):
-        flush_buffer()
-
-        self.list_buffer_hosts.assert_called_once_with()
-
-    def test_flush_buffer_gets_file_index_for_each_host(self):
-        flush_buffer()
-        
-        self.assertEqual(map(call, self.fake_hosts), self.file_index_for_host.mock_calls)
-
-    def test_flush_buffer_purges_buffer_with_all_found_checksums(self):
-        flush_buffer()
-
-        self.assertEqual(
-            call(('checksum1', 'checksum2', 'checksum3')),
-            self.purge_buffer.call_args
-        )
 
 class TestSyncToInbox(TestCase):
     def setUp(self):
@@ -128,33 +89,6 @@ class TestSyncToInbox(TestCase):
         self.assertIsInstance(ret, list)
         self.assertNotIsInstance(ret, GeneratorType)
 
-
-class TestListBufferHosts(TestCase):
-    def setUp(self):
-        patcher = patch('messor.drop_off.os')
-        self.addCleanup(patcher.stop)
-        self.mock_os = patcher.start()
-
-        patcher = patch('messor.drop_off.list_directories')
-        self.addCleanup(patcher.stop)
-        self.list_directories = patcher.start()
-
-    def test_list_buffer_hosts_joins_path(self):
-        list_buffer_hosts()
-
-        self.mock_os.path.join.assert_called_once_with(MESSOR_BUFFER, 'hosts')
-
-    def test_list_buffer_hosts_lists_directories(self):
-        list_buffer_hosts()
-
-        self.list_directories.assert_called_once_with(
-            self.mock_os.path.join.return_value
-        )
-
-    def test_list_buffer_hosts_returns_list_of_directories(self):
-        ret = list_buffer_hosts()
-
-        self.assertEqual(ret, self.list_directories.return_value)
 
 class TestProcessHost(TestCase):
     def setUp(self):
